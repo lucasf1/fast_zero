0a85f82cbbac7663e9b50d00773cf8fa26e9d042
Lucas Farias <lucasf1@gmail.com>
2024-05-15 21:56:31 -0300

    Adicionada a primeira migração com Alembic. Criada tabela de usuários.

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,94 +2,94 @@ from http import HTTPStatus
 
 
 def test_root_deve_retornar_ok_e_ola_mundo(client):
-    response = client.get("/")
+    response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {"message": "Olá Mundo!"}
+    assert response.json() == {'message': 'Olá Mundo!'}
 
 
 def test_create_user(client):
     response = client.post(
-        "/users/",
+        '/users/',
         json={
-            "username": "alice",
-            "email": "alice@example.com",
-            "password": "secret",
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
         },
     )
 
     assert response.status_code == HTTPStatus.CREATED
     assert response.json() == {
-        "username": "alice",
-        "email": "alice@example.com",
-        "id": 1,
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
     }
 
 
 def test_read_users(client):
-    response = client.get("/users/")
+    response = client.get('/users/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {
-        "users": [
+        'users': [
             {
-                "username": "alice",
-                "email": "alice@example.com",
-                "id": 1,
+                'username': 'alice',
+                'email': 'alice@example.com',
+                'id': 1,
             }
         ]
     }
 
 
 def test_read_user(client):
-    response = client.get("/users/1")
+    response = client.get('/users/1')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {
-        "username": "alice",
-        "email": "alice@example.com",
-        "id": 1,
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
     }
 
 
 def test_update_user(client):
     response = client.put(
-        "/users/1",
+        '/users/1',
         json={
-            "username": "bob",
-            "email": "bob@example.com",
-            "password": "mynewpassword",
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
         },
     )
 
     assert response.status_code == HTTPStatus.OK
 
     assert response.json() == {
-        "username": "bob",
-        "email": "bob@example.com",
-        "id": 1,
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': 1,
     }
 
 
 def test_update_user_invalid_id(client):
     response = client.put(
-        "/users/10",
+        '/users/10',
         json={
-            "username": "bob",
-            "email": "bob@example.com",
-            "password": "mynewpassword",
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
         },
     )
     assert response.status_code == HTTPStatus.NOT_FOUND
 
 
 def test_delete_user(client):
-    response = client.delete("/users/1")
+    response = client.delete('/users/1')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {"message": "User deleted"}
+    assert response.json() == {'message': 'User deleted'}
 
 
 def test_delete_user_invalid_id(client):
-    response = client.delete("/users/10")
+    response = client.delete('/users/10')
     assert response.status_code == HTTPStatus.NOT_FOUND
