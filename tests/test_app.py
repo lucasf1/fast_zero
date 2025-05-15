c440ca4f52f00d8e6b38eaa4aa9a173ac1ba0715
Lucas Farias <lucasf1@gmail.com>
2025-05-15 05:54:14 -0300

    resolução dos exercícios da aula 2

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,9 +6,31 @@ from fast_zero.app import app
 
 
 def test_root_deve_retornar_ok_e_ola_mundo():
+    # arrange
     client = TestClient(app)
 
+    # act
     response = client.get('/')
 
+    # assert
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_ola_mundo_html():
+    client = TestClient(app)
+    response = client.get('/ola-mundo')
+
+    assert response.status_code == HTTPStatus.OK
+    assert (
+        response.text
+        == """
+    <html>
+      <head>
+        <title>Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo!</h1>
+      </body>
+    </html>"""
+    )
